f72f30f992afada434dd6040f3e678445813f141
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-07-13 16:30:45 -0400

    Implemented dictionary selection and manual naming of dictionaries.

--- Commander.java
+++ Commander.java
@@ -1,5 +1,36 @@
 package nil;
 
+import java.util.Scanner;
+
 public class Commander {
 
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		while(!input.toLowerCase().equals("exit")){
+			
+			String[] inputTok = input.split(" ");
+			
+			switch(inputTok[0].toLowerCase()){
+				case "select":
+					selectLanguage(inputTok[1]);
+					break;
+				default:
+			}
+			
+			input = scan.next();
+			
+		}
+		
+		scan.close();
+		
+	}
+	
+	public static void selectLanguage(String languageName){
+		
+		Dictionary.connect(languageName);
+		
+	}
+	
 }
--- Dictionary.java
+++ Dictionary.java
@@ -1,5 +1,23 @@
 package nil;
 
+import java.sql.*;
+
 public class Dictionary {
 
+	private static Connection c;
+	private static boolean connected;
+	
+	public static void connect(String dictName){
+		
+		c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:" + dictName + ".db");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+	}
+	
 }
